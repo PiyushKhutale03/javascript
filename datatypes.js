3046d0613a05d5e62ee517be2c9514d19bb8fce8
PiyushKhutale03 <piyushkhutale33@gmail.com>
2026-02-02 15:00:05 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+
+// 1. Number
+let num = 100;
+console.log("Number:", num);
+
+// 2. String
+let str = "Hello JavaScript";
+console.log("String:", str);
+
+// 3. Boolean
+let isValid = true;
+console.log("Boolean:", isValid);
+
+// 4. Undefined
+let notDefined;
+console.log("Undefined:", notDefined);
+
+// 5. Null
+let emptyValue = null;
+console.log("Null:", emptyValue);
+
+
+// 6. Symbol
+let uniqueId = Symbol("id");
+console.log("Symbol:", uniqueId);
